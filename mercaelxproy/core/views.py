a0479d8dd73619059d299e8ebd3718eba4af5f82
PedroMsgr <pedrormsgr@gmail.com>
2024-11-13 16:40:48 +0100

    Paginacion de core y directori_comercios terminada

--- mercaelxproy/core/views.py
+++ mercaelxproy/core/views.py
@@ -7,6 +7,9 @@ class ProvinciaListView(ListView):
     model = Provincia
     template_name = 'core/provincia_list.html'
     context_object_name = 'object_list'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,23 +20,38 @@ class ProvinciaListView(ListView):
 class ProvinciaDetailView(DetailView):
     model = Provincia
     template_name = 'core/provincia_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
+    paginate_by = 3
 
 
 class CiudadListView(ListView):
     model = Ciudad
     template_name = 'core/ciudad_list.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
+    paginate_by = 3
 
 
 class CiudadDetailView(DetailView):
     model = Ciudad
     template_name = 'core/ciudad_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
+    paginate_by = 3
 
 
 class DistritoListView(ListView):
     model = Distrito
     template_name = 'core/distrito_list.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
+    paginate_by = 3
 
 
 class DistritoDetailView(DetailView):
     model = Distrito
     template_name = 'core/distrito_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
+    paginate_by = 3
--- mercaelxproy/directorio_comercios/views.py
+++ mercaelxproy/directorio_comercios/views.py
@@ -10,39 +10,55 @@ from directorio_comercios.models import Categoria, Subcategoria, Asociacion, Com
 class CategoriaListView(ListView): # ListView es una clase de Django que muestra una lista de objetos de un modelo
     model = Categoria
     template_name = 'directorio_comercios/categoria_list.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class CategoriaDetailView(DetailView): # DetailView es una clase de Django que muestra los detalles de un objeto de un modelo
     model = Categoria
     template_name = 'directorio_comercios/categoria_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class SubCategoriaListView(ListView):
     model = Subcategoria
     template_name = 'directorio_comercios/subcategoria_list.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class SubCategoriaDetailView(DetailView):
     model = Subcategoria
     template_name = 'directorio_comercios/subcategoria_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class AsociacionListView(ListView):
     model = Asociacion
     template_name = 'directorio_comercios/asociacion_list.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class AsociacionDetailView(DetailView):
     model = Asociacion
     template_name = 'directorio_comercios/asociacion_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class ComercioListView(ListView):
     model = Comercio
     template_name = 'directorio_comercios/comercio_list.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
 
 class ComercioDetailView(DetailView):
     model = Comercio
     template_name = 'directorio_comercios/comercio_detail.html'
+    #----UD6.8.b----
+    #pagina_by que sirve para paginar los resultados usando pagination.html
     paginate_by = 3
