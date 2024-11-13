a60c2be42898ebdc5e514ff9bfe338c2eeb09432
PedroMsgr <pedrormsgr@gmail.com>
2024-11-13 16:35:52 +0100

    Paginacion

--- mercaelxproy/directorio_comercios/views.py
+++ mercaelxproy/directorio_comercios/views.py
@@ -10,31 +10,39 @@ from directorio_comercios.models import Categoria, Subcategoria, Asociacion, Com
 class CategoriaListView(ListView): # ListView es una clase de Django que muestra una lista de objetos de un modelo
     model = Categoria
     template_name = 'directorio_comercios/categoria_list.html'
+    paginate_by = 3
 
 class CategoriaDetailView(DetailView): # DetailView es una clase de Django que muestra los detalles de un objeto de un modelo
     model = Categoria
     template_name = 'directorio_comercios/categoria_detail.html'
+    paginate_by = 3
 
 class SubCategoriaListView(ListView):
     model = Subcategoria
     template_name = 'directorio_comercios/subcategoria_list.html'
+    paginate_by = 3
 
 class SubCategoriaDetailView(DetailView):
     model = Subcategoria
     template_name = 'directorio_comercios/subcategoria_detail.html'
+    paginate_by = 3
 
 class AsociacionListView(ListView):
     model = Asociacion
     template_name = 'directorio_comercios/asociacion_list.html'
+    paginate_by = 3
 
 class AsociacionDetailView(DetailView):
     model = Asociacion
     template_name = 'directorio_comercios/asociacion_detail.html'
+    paginate_by = 3
 
 class ComercioListView(ListView):
     model = Comercio
     template_name = 'directorio_comercios/comercio_list.html'
+    paginate_by = 3
 
 class ComercioDetailView(DetailView):
     model = Comercio
     template_name = 'directorio_comercios/comercio_detail.html'
+    paginate_by = 3
